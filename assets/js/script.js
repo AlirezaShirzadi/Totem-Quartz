5125a84acfe61260949d15bdcc1dd870299dab5c
alirezashirzadi <shirzadi.alireza.9821160025@gmail.com>
2024-03-16 21:04:25 +0330

    fix: issues

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,16 @@
-AOS.init({disable: 'mobile'});
+AOS.init({ disable: "mobile", once: true });
 
 $(window).on("scroll", function () {
-    var scrollYpos = $(document).scrollTop();
-    var navHeight = $('#main-header').height();
-    if (scrollYpos > navHeight) {
-        $("#main-header").addClass('sticky')
-    } else {
-        $("#main-header").removeClass('sticky')
-    }
-})
+  var scrollYpos = $(document).scrollTop();
+  var navHeight = $("#main-header").height();
+  if (scrollYpos > navHeight) {
+    $("#main-header").addClass("sticky");
+  } else {
+    $("#main-header").removeClass("sticky");
+  }
+});
+
+$(".btn-search").on("click", function () {
+  $(".search-input").toggleClass("show");
+  $(this).toggleClass("on");
+});
