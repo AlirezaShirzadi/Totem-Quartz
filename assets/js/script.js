6fa85204c5b287119cd03eb5017c67913e54f0da
alirezashirzadi <shirzadi.alireza.9821160025@gmail.com>
2024-03-16 16:06:56 +0330

    fix: bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
 AOS.init({disable: 'mobile'});
+
+$(window).on("scroll", function () {
+    var scrollYpos = $(document).scrollTop();
+    var navHeight = $('#main-header').height();
+    if (scrollYpos > navHeight) {
+        $("#main-header").addClass('sticky')
+    } else {
+        $("#main-header").removeClass('sticky')
+    }
+})
