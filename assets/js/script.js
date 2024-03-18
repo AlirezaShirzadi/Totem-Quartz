a0eda01cc8f36720c53583423ebd0c359f224ef6
alirezashirzadi <shirzadi.alireza.9821160025@gmail.com>
2024-03-18 18:13:25 +0330

    fix: issues

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,12 +5,31 @@ $(window).on("scroll", function () {
   var navHeight = $("#main-header").height();
   if (scrollYpos > navHeight) {
     $("#main-header").addClass("sticky");
+    $(".dropdown-full-size .dropdown-menu").css({
+      'top' : '77px',
+      'width' : '100%',
+      'margin-right' : '0'
+    })
   } else {
     $("#main-header").removeClass("sticky");
+    $(".dropdown-full-size .dropdown-menu").css({
+      'top' : '110px',
+      'width' : 'calc(100% - 60px)',
+      'margin-right' : '30px'
+    })
   }
+  // $('.open').removeClass('.open');
 });
 
 $(".btn-search").on("click", function () {
   $(".search-input").toggleClass("show");
   $(this).toggleClass("on");
+});
+
+$(".dropdown-full-size").on("mouseenter", function () {
+  $(this).$(".dropdown-menu").toggleClass("open");
+});
+
+$(".dropdown-full-size").on("mouseleave", function () {
+  $(this).$(".dropdown-menu").toggleClass("open");
 });
