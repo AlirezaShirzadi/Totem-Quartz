53ee48e2c19fc04b88a5f616846ac7f68246d5d8
alirezashirzadi <shirzadi.alireza.9821160025@gmail.com>
2024-03-24 21:29:36 +0330

    fix: headet issues

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,34 +2,31 @@ AOS.init({ disable: "mobile", once: true });
 
 $(window).on("scroll", function () {
   var scrollYpos = $(document).scrollTop();
+  var scroll = $(window).scrollTop();
   var navHeight = $("#main-header").height();
   if (scrollYpos > navHeight) {
     $("#main-header").addClass("sticky");
     $(".dropdown-full-size .dropdown-menu").css({
-      'top' : '77px',
-      'width' : '100%',
-      'margin-right' : '0'
-    })
+      top: "77px",
+      width: "100%",
+      "margin-right": "0",
+    });
   } else {
     $("#main-header").removeClass("sticky");
     $(".dropdown-full-size .dropdown-menu").css({
-      'top' : '110px',
-      'width' : 'calc(100% - 60px)',
-      'margin-right' : '30px'
-    })
+      top: "108px",
+      width: "calc(100% - 60px)",
+      "margin-right": "30px",
+    });
   }
-  // $('.open').removeClass('.open');
+  $(".dropdown-menu").addClass("hidden");
+  clearTimeout($.data(this, 'scrollTimer'));
+  $.data(this, 'scrollTimer', setTimeout(function() {
+		$(".dropdown-menu").removeClass('hidden');
+	}, 250));
 });
 
 $(".btn-search").on("click", function () {
   $(".search-input").toggleClass("show");
   $(this).toggleClass("on");
-});
-
-$(".dropdown-full-size").on("mouseenter", function () {
-  $(this).$(".dropdown-menu").toggleClass("open");
-});
-
-$(".dropdown-full-size").on("mouseleave", function () {
-  $(this).$(".dropdown-menu").toggleClass("open");
 });
